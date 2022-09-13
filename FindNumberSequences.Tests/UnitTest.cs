5a72070cc56ffd91cafdfe092b869041a783787d
SimonNyvall <simon.k.nyvall@gmail.com>
2022-09-13 11:07:12 +0200

    Added xunit test to the project and safe guards to check if parameters is null or empry

---
+++ FindNumberSequences.Tests/UnitTest.cs
@@ -0,0 +1,123 @@
+using FindNumberSequences;
+
+namespace FindNumberSequences.Tests
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void GetNextIndexForMachingNumber_TestNormalCase()
+        {
+            const string input = "29535123p48723487597645723645";
+            int startIndex = 0;
+
+            int expected = 6;
+
+            int actual = Lab.GetNextIndexForMachingNumber(input, startIndex);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void GetNextIndexForMachingNumber_TestOnlyLetters()
+        {
+            const string input = "pppppppppppppppppppppppppppp";
+            int startIndex = 0;
+
+            int expected = 1;
+
+            int actual = Lab.GetNextIndexForMachingNumber(input, startIndex);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void GetNextIndexForMachingNumber_TestNullCase()
+        {
+            const string input = null;
+            int startIndex = 0;
+
+            int expected = -1;
+
+            int actual = Lab.GetNextIndexForMachingNumber(input, startIndex);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void GetNextIndexForMachingNumber_TestEmptyCase()
+        {
+            string input = String.Empty;
+            int startIndex = 0;
+
+            int expected = -1;
+
+            int actual = Lab.GetNextIndexForMachingNumber(input, startIndex);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void GetSubstringFromStartToEndIndex_TestNormalCase()
+        {
+            string input = "29535123p48723487597645723645";
+            int start = 0;
+            int end = 6;
+
+            string expected = "2953512";
+
+            string actual = Lab.GetSubstringFromStartToEndIndex(input, start, end);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void GetSubstringFromStartToEndIndex_TestNullCase()
+        {
+            string input = null;
+            int start = 0;
+            int end = 6;
+
+            Action nullTest = () => Lab.GetSubstringFromStartToEndIndex(input, start, end);
+            Assert.Throws<ArgumentException>(nullTest);                      
+        }
+        public void GetSubstringFromStartToEndIndex_TestEmptyCase()
+        {
+            string input = String.Empty;
+            int start = 0;
+            int end = 6;
+
+            Action emptyTest = () => Lab.GetSubstringFromStartToEndIndex(input, start, end);
+            Assert.Throws<ArgumentException>(emptyTest);
+        }
+        [Fact]
+        public void DoesStringOnlyContainNumbers_TestOnlyNumbers()
+        {
+            string outputSubstring = "456329563";
+
+            Assert.True(Lab.DoesStringOnlyContainNumbers(outputSubstring));
+        }
+        [Fact]
+        public void DoesStringOnlyContainNumbers_TestOnlyLetters()
+        {
+            string outputSubstring = "eitwoper";
+
+            Assert.False(Lab.DoesStringOnlyContainNumbers(outputSubstring));
+        }
+        [Fact]
+        public void DoesStringOnlyContainNumbers_TestNumberAndLetters()
+        {
+            string outputSubstring = "21342p13312";
+
+            Assert.False(Lab.DoesStringOnlyContainNumbers(outputSubstring));
+        }
+        [Fact]
+        public void DoesStringOnlyContainNumbers_TestNullCase()
+        {
+            string outputSubstring = null;
+
+            Assert.False(Lab.DoesStringOnlyContainNumbers(outputSubstring));
+        }
+        [Fact]
+        public void DoesStringOnlyContainNumbers_TestEmptyCase()
+        {
+            string outputSubstring = String.Empty;
+
+            Assert.False(Lab.DoesStringOnlyContainNumbers(outputSubstring));
+        }
+    }
+}
---
+++ Lab1/FindNumberSequences.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FindNumberSequences
+{
+    public class Lab
+    {
+        public static void Main(string[] args)
+        {
+            const string input = "29535123p48723487597645723645";
+            if (string.IsNullOrEmpty(input)) return;
+            
+            ulong substringSum = 0;
+
+            for (int startIndex = 0; startIndex < input.Length; startIndex++)
+            {
+                int endIndex = GetNextIndexForMachingNumber(input, startIndex);
+                if (endIndex > 0)
+                {
+                    string outputSubstring = GetSubstringFromStartToEndIndex(input, startIndex, endIndex);
+
+                    if (DoesStringOnlyContainNumbers(outputSubstring))
+                    {
+                        substringSum += ulong.Parse(outputSubstring);
+
+                        PrintColoredSubstring(input, startIndex, outputSubstring.Length);
+                    }
+                }
+            }
+            Console.WriteLine($"\nThe sum is: {substringSum}");
+        }
+        public static int GetNextIndexForMachingNumber(string input, int startIndex)
+        {
+            if (string.IsNullOrEmpty(input)) return -1;
+            if (startIndex < 0 || startIndex > input.Length) return -1;
+            
+            return input.IndexOf(input[startIndex], startIndex + 1);
+        }
+
+        public static string GetSubstringFromStartToEndIndex(string input, int start, int end)
+        {
+            if (string.IsNullOrEmpty(input)) throw new ArgumentException("Empty or null", "input");
+            if (start < 0 || end > input.Length) throw new ArgumentException("Indeces was outside the length of input");
+            
+            return input.Substring(start, end - start + 1);
+        }
+
+        public static bool DoesStringOnlyContainNumbers(string outputSubstring)
+        {
+            if (string.IsNullOrEmpty(outputSubstring)) return false;
+
+            return outputSubstring.All(c => char.IsDigit(c));          
+        }
+
+        private static void PrintColoredSubstring(string input, int start, int length)
+        {           
+            Console.Write(input.Substring(0, start));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(input.Substring(start, length));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(input.Substring(start + length));
+        }
+    }
+}
